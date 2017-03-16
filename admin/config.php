8a767a3f46ac81a70b60de531a9f86a16558dda4
Karel Trnka <Karel Trnka>
2017-03-16 01:52:33 +0100

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/kk/admin/');
+define('HTTP_CATALOG', 'http://localhost/kk/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/kk/admin/');
+define('HTTPS_CATALOG', 'http://localhost/kk/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/prace/xampp/htdocs/kk/admin/');
+define('DIR_SYSTEM', 'D:/prace/xampp/htdocs/kk/system/');
+define('DIR_IMAGE', 'D:/prace/xampp/htdocs/kk/image/');
+define('DIR_LANGUAGE', 'D:/prace/xampp/htdocs/kk/admin/language/');
+define('DIR_TEMPLATE', 'D:/prace/xampp/htdocs/kk/admin/view/template/');
+define('DIR_CONFIG', 'D:/prace/xampp/htdocs/kk/system/config/');
+define('DIR_CACHE', 'D:/prace/xampp/htdocs/kk/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/prace/xampp/htdocs/kk/system/storage/download/');
+define('DIR_LOGS', 'D:/prace/xampp/htdocs/kk/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/prace/xampp/htdocs/kk/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/prace/xampp/htdocs/kk/system/storage/upload/');
+define('DIR_CATALOG', 'D:/prace/xampp/htdocs/kk/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'kk');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
